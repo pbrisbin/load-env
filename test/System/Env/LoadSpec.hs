aad9174e61aab17f7bf6976007e9a1c77afcae3e
patrick brisbin <pbrisbin@gmail.com>
2014-07-18 14:50:47 -0400

    Use specific env-file in spec, cleanup after

--- test/System/Env/LoadSpec.hs
+++ test/System/Env/LoadSpec.hs
@@ -1,25 +1,35 @@
 module System.Env.LoadSpec (main, spec) where
 
-import Test.Hspec
+import Control.Monad (when)
+import System.Directory (doesFileExist, removeFile)
 import System.Environment (lookupEnv)
 
+import Test.Hspec
 import System.Env.Load
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = do
+spec = after cleanup $ do
     describe "loadEnv" $ do
         it "loads environment variables from ./.env if present" $ do
-            writeFile ".env" $ unlines
+            writeFile envFile $ unlines
                 [ "FOO=\"bar\""
                 , "BAZ=\"bat\""
                 ]
 
-            loadEnv
+            loadEnvFrom envFile
 
             mbar <- lookupEnv "FOO"
             mbat <- lookupEnv "BAZ"
             mbar `shouldBe` Just "bar"
             mbat `shouldBe` Just "bat"
+
+cleanup :: IO ()
+cleanup = do
+    e <- doesFileExist envFile
+    when e $ removeFile envFile
+
+envFile :: FilePath
+envFile = "/tmp/load-env-test-file"
