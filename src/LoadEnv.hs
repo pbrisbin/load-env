078e477b5b3e246aedfd166660254151fb1eb50d
patrick brisbin <pbrisbin@gmail.com>
2018-05-24 16:55:44 -0400

    Adjust documentation

--- src/LoadEnv.hs
+++ src/LoadEnv.hs
@@ -1,3 +1,22 @@
+-- |
+--
+-- This is effectively a port of dotenv, whose README explains it best:
+--
+-- /Storing configuration in the environment is one of the tenets of a
+-- twelve-factor app. Anything that is likely to change between deployment
+-- environments–such as resource handles for databases or credentials for
+-- external services–should be extracted from the code into environment
+-- variables./
+--
+-- /But it is not always practical to set environment variables on development
+-- machines or continuous integration servers where multiple projects are run.
+-- dotenv loads variables from a .env file into ENV when the environment is
+-- bootstrapped./
+--
+-- <https://github.com/bkeepers/dotenv>
+--
+-- This library exposes functions for doing just that.
+--
 module LoadEnv
     ( loadEnv
     , loadEnvFrom
@@ -10,29 +29,27 @@ import System.Directory (doesFileExist)
 import System.Environment (setEnv)
 import Text.Parsec.String (parseFromFile)
 
--- |
+-- | @'loadEnvFrom' \".env\"@
+loadEnv :: IO ()
+loadEnv = loadEnvFrom ".env"
+
+-- | Parse the given file and set variables in the process's environment
 --
--- Parse @./.env@ for variable declariations. Set those variables in the
--- currently running process's environment. Variables can be declared in the
--- following form:
+-- Variables can be declared in the following form:
 --
 -- > FOO=bar
 -- > FOO="bar"
 -- > FOO='bar'
 --
--- Declarations may optionally be preceded by @export@, which will be ignored.
--- Trailing whitespace is ignored. Quotes inside quoted values or spaces in
--- unquoted values must be escaped with a backlash.
+-- Declarations may optionally be preceded by @\"export \"@, which will be
+-- ignored. Trailing whitespace is ignored. Quotes inside quoted values or
+-- spaces in unquoted values must be escaped with a backlash. Invalid lines are
+-- silently ignored.
 --
--- Invalid lines are silently ignored.
+-- __NOTE__: If the file-name is relative, the directory tree will be traversed
+-- up to @\/@ looking for the file in each parent. Use @'loadEnvFromAbsolute'@
+-- to avoid this.
 --
--- If you wish to specify your own file, use @'loadEnvFrom'@. If you wish to
--- pass your own string or work with the parse result directly, use the
--- lower-level functions available in @"LoadEnv.Parse"@.
---
-loadEnv :: IO ()
-loadEnv = loadEnvFrom ".env"
-
 loadEnvFrom :: FilePath -> IO ()
 loadEnvFrom fp = do
     e <- doesFileExist fp
