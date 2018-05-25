4358b7d7c3df41399ba2f454d5b54d4d28ec0c45
patrick brisbin <pbrisbin@gmail.com>
2018-05-25 16:12:54 -0400

    Update doc

--- src/LoadEnv.hs
+++ src/LoadEnv.hs
@@ -2,16 +2,16 @@
 --
 -- This is effectively a port of dotenv, whose README explains it best:
 --
--- /Storing configuration in the environment is one of the tenets of a
--- twelve-factor app. Anything that is likely to change between deployment
--- environments–such as resource handles for databases or credentials for
--- external services–should be extracted from the code into environment
--- variables./
---
--- /But it is not always practical to set environment variables on development
--- machines or continuous integration servers where multiple projects are run.
--- dotenv loads variables from a .env file into ENV when the environment is
--- bootstrapped./
+-- > Storing configuration in the environment is one of the tenets of a
+-- > twelve-factor app. Anything that is likely to change between deployment
+-- > environments–such as resource handles for databases or credentials for
+-- > external services–should be extracted from the code into environment
+-- > variables.
+-- >
+-- > But it is not always practical to set environment variables on development
+-- > machines or continuous integration servers where multiple projects are run.
+-- > dotenv loads variables from a .env file into ENV when the environment is
+-- > bootstrapped.
 --
 -- <https://github.com/bkeepers/dotenv>
 --
